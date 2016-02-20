756a9032d45fd141eb881b019da8197b10871191
Gi60s <djK82hR#!E%KPpd>
2016-02-20 12:55:50 -0700

    update gulp file

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,5 +1,10 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
-gulp.task('default', function() {
-    console.log('Ran default task.');
+gulp.task('compress-js', function() {
+    return gulp.src(['src/*.js', 'src/**/*.js'])
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist'));
 });
